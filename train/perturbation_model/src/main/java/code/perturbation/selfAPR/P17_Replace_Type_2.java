101c48563560c5b5acb17c9302995b08ace0f038
chathuranga-jayanath <chathuranga.19@cse.mrt.ac.lk>
2023-12-26 00:54:33 +0530

    add perturbation model

---
+++ train/perturbation_model/src/main/java/code/perturbation/selfAPR/P17_Replace_Type_2.java
@@ -0,0 +1,27 @@
+package code.perturbation.selfAPR;
+
+import java.util.HashMap;
+
+import code.output.result.PerturbResult;
+import code.perturbation.OperatorPerturbation_bak;
+import code.perturbation.TypePerturbation;
+import code.perturbation.utils.StatementType;
+import spoon.reflect.declaration.CtElement;
+
+public class P17_Replace_Type_2 {
+    public static void perturb(CtElement st, StatementType type, int methStart, int methEnd, String groundTruth, int lineNo1,
+			String lineNo2, String lineNo3,String lineNo4,String lineNo5) {
+		String perturbCode = null;
+		String actionNo = "[P17_Replace_Type_2]";
+		
+		/**
+		 * type perturbation
+		 */
+		perturbCode = TypePerturbation.perturb(st, groundTruth);
+		
+		if(!groundTruth.equals(perturbCode) && perturbCode!=null ){
+			PerturbResult.parsePerturb(actionNo,perturbCode,methStart+"",methEnd+"",lineNo1+"",lineNo2,lineNo3,lineNo4,lineNo5,groundTruth);		
+		}		
+	}
+}
+
