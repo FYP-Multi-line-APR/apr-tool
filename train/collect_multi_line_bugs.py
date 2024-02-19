71d80c9a11aa22fa1589d365930bc79b57aeb031
chathuranga-jayanath <chathuranga.19@cse.mrt.ac.lk>
2024-02-19 14:52:32 +0530

    add scripts to collect all multi line from repos

--- train/collect_multi_line_bugs.py
+++ train/collect_multi_line_bugs.py
@@ -1,8 +1,7 @@
 import sys , random
 
-from utils import get_json_data, write_json
-
-mulitple_context_identification = "multiple-contexts"
+from utils import ctx_field
+from utils import get_json_data, write_json, make_dir
 
 # file_paths = [
 #     # "./PerturbedSamples/PerturbedJsons/set8-context-5-predict-token-for-fine-tune/train-data-multiple-contexts.json",
@@ -20,18 +19,31 @@ def get_amount_to_take(amount_to_take_input, file_content_length):
         return file_content_length
     return min(int(amount_to_take_input), file_content_length)
 
+def filter_max_context_size(content):
+    result = []
+    for data_element in content:
+        contexts = data_element[ctx_field]
+        crop_context_size = max_context_size if len(contexts) > max_context_size else len(contexts)
+        data_element[ctx_field] = data_element[ctx_field][:crop_context_size]
+        result.append(result)
+    return result
+
+mulitple_context_identification = "multiple-contexts"
+max_context_size = 2
+
 if __name__ == "__main__":
-    # file_path = "./PerturbedSamples/PerturbedJsons/set8-context-5-predict-token-for-fine-tune/train-data-multiple-contexts.json"
     result_dir = sys.argv[1]
     amount_to_take_input = sys.argv[2]
     file_paths = sys.argv[3:]
     multi_line_set_dir = f"./PerturbedSamples/PerturbedJsons/{result_dir}/"
+    make_dir(multi_line_set_dir)
     result_file_path = multi_line_set_dir + "collected-multiple-context-train-data.json"
     result = []
     for file_path in file_paths:
         if mulitple_context_identification in file_path:
             file_content = get_json_data(file_path)
             amount_to_take = get_amount_to_take(amount_to_take_input, len(file_content))
-            data_to_write = shuffle_and_take(file_content, amount_to_take)
+            shuffled_data = shuffle_and_take(file_content, amount_to_take)
+            data_to_write = filter_max_context_size(shuffled_data)
             result.extend(data_to_write)
     write_json(result_file_path, result)
--- train/collect_multi_line_bugs.sh
+++ train/collect_multi_line_bugs.sh
@@ -11,14 +11,15 @@
 #     ./PerturbedSamples/PerturbedJsons/set16-context-5-for-supercsv/train-data-without-comments-multiple-contexts.json 
 
 python3 collect_multi_line_bugs.py \
-    multi-line-set-3 \
-    10000 \
-    ./PerturbedSamples/PerturbedJsons/set9-context-5-predict-token-for-fine-tune-finmath/train-data-without-comments-multiple-contexts.json \
-    ./PerturbedSamples/PerturbedJsons/set10-context-5-prediction-token-for-time4j/train-data-without-comments-multiple-contexts.json \
-    ./PerturbedSamples/PerturbedJsons/set11-context-5-predict-token-for-html/train-data-without-comments-multiple-contexts.json \
-    ./PerturbedSamples/PerturbedJsons/set12-context-5-predict-token-for-maven-doxia/train-data-without-comments-multiple-contexts.json \
-    ./PerturbedSamples/PerturbedJsons/set14-context-5-for-wro4j/train-data-without-comments-multiple-contexts.json \
-    ./PerturbedSamples/PerturbedJsons/set15-context-5-for-guava/train-data-without-comments-multiple-contexts.json \
-    ./PerturbedSamples/PerturbedJsons/set16-context-5-for-supercsv/train-data-without-comments-multiple-contexts.json 
+    multi-line-set-4 \
+    all \
+    ./PerturbedSamples/PerturbedJsons/set8-context-5-for-rhino/train-data-without-comments-multiple-contexts-all-bugs.json \
+    ./PerturbedSamples/PerturbedJsons/set9-context-5-predict-token-for-fine-tune-finmath/train-data-without-comments-multiple-contexts-all-bugs.json \
+    ./PerturbedSamples/PerturbedJsons/set10-context-5-prediction-token-for-time4j/train-data-without-comments-multiple-contexts-all-bugs.json \
+    ./PerturbedSamples/PerturbedJsons/set11-context-5-predict-token-for-html/train-data-without-comments-multiple-contexts-all-bugs.json \
+    ./PerturbedSamples/PerturbedJsons/set12-context-5-predict-token-for-maven-doxia/train-data-without-comments-multiple-contexts-all-bugs.json \
+    ./PerturbedSamples/PerturbedJsons/set14-context-5-for-wro4j/train-data-without-comments-multiple-contexts-all-bugs.json \
+    ./PerturbedSamples/PerturbedJsons/set15-context-5-for-guava/train-data-without-comments-multiple-contexts-all-bugs.json \
+    ./PerturbedSamples/PerturbedJsons/set16-context-5-for-supercsv/train-data-without-comments-multiple-contexts-all-bugs.json 
 
 
