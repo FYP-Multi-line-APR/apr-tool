eb8d571088c2494132ce92eba23222ea172aa3ee
chathuranga-jayanath <chathuranga.19@cse.mrt.ac.lk>
2024-03-15 11:07:14 +0530

    collect context for context width 11

--- train/collect_context.py
+++ train/collect_context.py
@@ -2,8 +2,9 @@ import os, re
 
 context_jar_path = "./../utils/context.jar "
 prediction_token = "<extra_id_0>"
-bug_token = "[BUGGY]"
+bug_token = "[BUG]"
 context_token = "[CONTEXT]"
+context_width = 11
 
 def extract_startline_no(text):
     match = re.search(r'startline:(\d+)', text)
@@ -28,7 +29,6 @@ def extract_for_fine_tune(text):
 
 def execute_find_context(file_path, bug_line_no):
     result = os.popen("java -jar "+context_jar_path +file_path +" test-"+str(bug_line_no)).read()
-    # print(f"execute_find_context:{result}")
     return result
 
 def get_function_line_range(bug_file_path, bug_line):
@@ -40,22 +40,23 @@ def get_function_line_range(bug_file_path, bug_line):
 def get_function_content_with_prediction_token(file_path, stratline_no, endline_no, start_bug_line, end_bug_line):
     print(f"start_bug_line:{start_bug_line}, end_bug_line:{end_bug_line}")
     extracted_lines = []
+    buggy_lines = []
     with open(file_path, 'r', encoding='utf-8') as file:
         all_lines = file.readlines()
         stratline_no = max(1, min(stratline_no, len(all_lines)))
         endline_no = max(1, min(endline_no, len(all_lines)))
         for i in range(stratline_no - 1, endline_no):
-            
             if i == (start_bug_line - 1):
-                
                 extracted_lines.append(prediction_token)
+                buggy_lines.append(all_lines[i])
             elif i < start_bug_line:
                 extracted_lines.append(all_lines[i])
             elif start_bug_line - 1 < i and i < end_bug_line:
+                buggy_lines.append(all_lines[i])
                 continue
             else:
                 extracted_lines.append(all_lines[i])
-    return extracted_lines
+    return extracted_lines, buggy_lines
 
 def get_function_content(file_path, stratline_no, endline_no):
     try:
@@ -76,6 +77,49 @@ def get_function_content_without_comments(file_path, stratline_no, endline_no):
     content_with_comments = get_function_content(file_path, stratline_no, endline_no)
     content_without_comments = remove_comment_lines(content_with_comments)
     return get_content_lines_as_string(content_without_comments)
+
+def get_file_lines_with_prediction_token(file_path, start_bug_line, end_bug_line):
+    file_lines = []
+    with open(file_path, 'r', encoding='utf-8') as file:
+        all_lines = file.readlines()
+        for i in range(0, len(all_lines)):
+            if i == (start_bug_line - 1):
+                file_lines.append(prediction_token)
+            elif i < start_bug_line:
+                all_lines[i] = all_lines[i].replace('\t',' ').replace('\n',' ').replace('\r',' ').replace('  ',' ')
+                file_lines.append(all_lines[i])
+            elif start_bug_line - 1 < i and i < end_bug_line:
+                continue
+            else:
+                all_lines[i] = all_lines[i].replace('\t',' ').replace('\n',' ').replace('\r',' ').replace('  ',' ')
+                file_lines.append(all_lines[i])
+    return file_lines
+
+def filter_context(func_lines):
+    global context_width
+    predicting_line_index = -1 
+    for i in range(len(func_lines)):
+        line = func_lines[i]
+        if prediction_token in line:
+            predicting_line_index = i 
+            break 
+    estimate_start = predicting_line_index - 1 - context_width
+    estimate_end = predicting_line_index + 1 + context_width
+    start_index = max(0, estimate_start) 
+    end_index = min(len(func_lines) - 1, estimate_end)
+    return func_lines[start_index:end_index + 1]
+
+def get_context_with_prediction_token_without_comments(file_path, start_bug_line, end_bug_line):
+    file_lines = get_file_lines_with_prediction_token(file_path, start_bug_line, end_bug_line)
+    file_lines_without_comment = remove_comment_lines(file_lines)
+    filtered_content = filter_context(file_lines_without_comment)
+    return get_content_lines_as_string(filtered_content)
+
+def get_full_file_context_with_prediction_token_without_comments(file_path, start_bug_line, end_bug_line):
+    file_lines = get_file_lines_with_prediction_token(file_path, start_bug_line, end_bug_line)
+    file_lines_without_comment = remove_comment_lines(file_lines)
+    split_list = [get_content_lines_as_string(file_lines_without_comment[i:i+context_width]) for i in range(0, len(file_lines_without_comment), context_width)]
+    return split_list
 
 def remove_comment_lines(content_lines):
     content_without_comments = []
@@ -111,24 +155,36 @@ def add_bug_token_for_func(lines, func_start, bug_start, bug_end):
             result.append(line)        
     return result
 
-def collect_context(file_path, start_bug_line, end_bug_line):
+def collect_buggy_lines(func_lines, func_start, func_end, bug_start, bug_end):
+    result = []
+    for i in range(len(func_lines)):
+        line = func_lines[i]
+        curr_line = func_start + i 
+        if bug_start <= curr_line and curr_line <= bug_end:
+            result.append(line)
+    return process_lines(result)
+
+def collect_context(file_path, start_bug_line, end_bug_line, corrupt_code):
     func_start, func_end = get_function_line_range(file_path, start_bug_line)
-    
-    func_lines = get_function_content(file_path, func_start, func_end)
-    func_lines = add_bug_token_for_func(func_lines, func_start, start_bug_line, end_bug_line)
+    # func_lines = get_function_content(file_path, func_start, func_end)
+    func_lines, buggy_lines = get_function_content_with_prediction_token(file_path, func_start, func_end, start_bug_line, end_bug_line)
+    # func_lines = add_bug_token_for_func(func_lines, func_start, start_bug_line, end_bug_line)
     func_lines = remove_comment_lines(func_lines)
-    context_info = get_content_lines_as_string(func_lines)
-    
-    class_info = execute_find_context(file_path, start_bug_line)
-    class_info = extract_for_fine_tune(class_info)
-    return  f"{context_token} {context_info} {class_info}"
+    # print(f"buggy_lines:{buggy_lines}")
+    buggy_lines_str = process_lines(buggy_lines)
+    # limit context 
+    truncated_lines = func_lines
+    truncated_lines_str = process_lines(truncated_lines)
+    return  f"{bug_token} {corrupt_code} {context_token} {truncated_lines_str}"
 
 def process_lines(lines):
     result = []
     for line in lines:
-        if process_line(line) != '':
-            result.append(line)
-    return result
+        processed_line = process_line(line)
+        if processed_line != '':
+            result.append(processed_line)
+    # print(f"process lines : {result}")
+    return ' '.join(result)
 
 def process_line(line):
     line = line.strip()
@@ -141,9 +197,5 @@ def process_line(line):
 if __name__ == "__main__":
     bug_file_path = "./../train/PerturbedSamples/Csv-1/src/main/java/org/apache/maven/doxia/DefaultConverter.java"
     bug_line = 188
-
     context = collect_context(bug_file_path, 188, 189)
-    # result = execute_find_context(bug_file_path, bug_line)
-    # finetune_result = extract_for_fine_tune(result)
     print(context)
-    
